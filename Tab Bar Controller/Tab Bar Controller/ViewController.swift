6e04f5a18c082effa219bfa37ad77e1170948a7f
Krish_Bahukhandi <157385921+sub1ne@users.noreply.github.com>
2025-07-31 10:06:55 +0530

    Add files via upload

---
+++ Tab Bar Controller/Tab Bar Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tab Bar Controller
+//
+//  Created by Student on 30/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Tab Bar Controller/Tab Bar ControllerTests/Tab_Bar_ControllerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tab_Bar_ControllerTests.swift
+//  Tab Bar ControllerTests
+//
+//  Created by Student on 30/07/25.
+//
+
+import Testing
+@testable import Tab_Bar_Controller
+
+struct Tab_Bar_ControllerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
